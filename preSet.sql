3181f9367214466cb47b0d280299897d98273d9d
Xavier Antonio Mas <70808503+xavi956@users.noreply.github.com>
2025-03-26 07:53:14 +0100

    Create preSet.sql

---
+++ preSet.sql
@@ -0,0 +1,37 @@
+INSERT INTO Libro (titulo, autor, editorial, genero, isbn, paginas, precio) 
+VALUES 
+('Cien años de soledad', 'Gabriel García Márquez', 'Sudamericana', 'Novela', '978-3-16-148410-0', 432, 18.50),
+('1984', 'George Orwell', 'Secker & Warburg', 'Distopía', '978-0-452-28423-4', 328, 15.99),
+('El Principito', 'Antoine de Saint-Exupéry', 'Gallimard', 'Fábula', '978-0-123-45678-9', 96, 10.50),
+('Don Quijote de la Mancha', 'Miguel de Cervantes', 'Francisco de Robles', 'Novela', '978-84-376-0494-7', 863, 22.75),
+('La sombra del viento', 'Carlos Ruiz Zafón', 'Planeta', 'Misterio', '978-84-080-8632-0', 560, 19.95);
+
+--
+
+INSERT INTO CD (titulo, artista, genero, canciones, precio) 
+VALUES 
+('Thriller', 'Michael Jackson', 'Pop', 9, 12.99),
+('Back in Black', 'AC/DC', 'Rock', 10, 14.99),
+('The Dark Side of the Moon', 'Pink Floyd', 'Rock', 10, 13.50),
+('Rumours', 'Fleetwood Mac', 'Rock', 11, 11.99),
+('Abbey Road', 'The Beatles', 'Rock', 17, 15.99);
+
+--
+
+INSERT INTO DVD (titulo, director, genero, duracion, precio) 
+VALUES 
+('El Padrino', 'Francis Ford Coppola', 'Crimen', 175, 9.99),
+('Matrix', 'Lana Wachowski', 'Ciencia Ficción', 136, 11.99),
+('Inception', 'Christopher Nolan', 'Ciencia Ficción', 148, 10.99),
+('Forrest Gump', 'Robert Zemeckis', 'Drama', 142, 8.99),
+('The Lord of the Rings: The Fellowship of the Ring', 'Peter Jackson', 'Fantasía', 178, 12.99);
+
+--
+
+INSERT INTO JuegoPlayStation (titulo, desarrollador, genero, plataforma, edadRecomendada, precio) 
+VALUES 
+('The Last of Us', 'Naughty Dog', 'Aventura', 'PS4', 18, 49.99),
+('God of War', 'Santa Monica Studio', 'Acción', 'PS5', 18, 59.99),
+('Uncharted 4', 'Naughty Dog', 'Acción/Aventura', 'PS4', 16, 39.99),
+('Spider-Man', 'Insomniac Games', 'Acción', 'PS4', 16, 44.99),
+('Horizon Zero Dawn', 'Guerrilla Games', 'Acción/RPG', 'PS4', 16, 34.99);
