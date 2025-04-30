5a912135ccb98265878810b1e7a955df86715bbd
Xavier Antonio Mas <70808503+xavi956@users.noreply.github.com>
2025-04-30 08:02:38 +0200

    Update preSet.sql

--- preSet.sql
+++ preSet.sql
@@ -1,31 +1,23 @@
-INSERT INTO Libro (titulo, autor, editorial, genero, isbn, paginas, precio) 
-VALUES 
-('La belleza del objeto cotidiano', 'Soetsu Yanagi', 'gg', 'Ensayo diseño', '9788425232985', 240, 14.15),
-('1984', 'George Orwell', 'Secker & Warburg', 'Distopía', '9780452284234', 328, 15.99),
-('Momentos estelares de la humanidad', 'Stefan Zweig', 'Acantilado', 'Ensayo histórico', '9788495359926', 96, 18.50),
-('Don Quijote de la Mancha', 'Miguel de Cervantes', 'Francisco de Robles', 'Novela', '9788437604947', 863, 22.75),
-('El rey Lear', 'William Shakespeare', 'Austral', 'Tragedia', '9788467028423', 208, 7.55);
+INSERT INTO Prestamos (
+    tipo_producto, id_producto, fecha_prestamo, fecha_devolucion, nombre_usuario
+) VALUES (
+    'Libro', 1, '2025-04-20 10:30:00', '2025-05-20 10:30:00', 'Juan Pérez'
+);
 
-INSERT INTO CD (titulo, artista, genero, canciones, precio) 
-VALUES 
-('Yo, minoría absoluta', 'Extremoduro', 'Rock', 10, 8.99),
-('Back in Black', 'AC/DC', 'Rock', 10, 14.99),
-('The Number of the Beast', 'Iron Maiden', 'Heavy Metal', 8, 14.99),
-('Jugar al gua', 'Rosendo', 'Rock', 9, 21.99),
-('Ciudad de las ideas', 'Vicente Amigo', 'Flamenco', 8, 17.99);
+INSERT INTO Prestamos (
+    tipo_producto, id_producto, fecha_prestamo, fecha_devolucion, nombre_usuario
+) VALUES (
+    'CD', 2, '2025-04-22 15:00:00', '2025-05-06 15:00:00', 'Laura Gómez'
+);
 
-INSERT INTO DVD (titulo, director, genero, duracion, precio) 
-VALUES 
-('El Padrino', 'Francis Ford Coppola', 'Crimen', 175, 9.99),
-('Ciudad de Dios', 'Fernando Meirelles', 'Drama', 129, 5.99),
-('Memorias de un asesino', 'Bong Joon Ho', 'Crimen', 131, 10.99),
-('La Haine', 'Mathieu Kassovitz', 'Drama', 98, 12.99),
-('Trainspotting', 'Danny Boyle', 'Drama', 94, 9.99);
+INSERT INTO Prestamos (
+    tipo_producto, id_producto, fecha_prestamo, fecha_devolucion, nombre_usuario
+) VALUES (
+    'DVD', 3, '2025-04-25 12:00:00', '2025-05-02 12:00:00', 'Carlos Ruiz'
+);
 
-INSERT INTO JuegoPlayStation (titulo, desarrollador, genero, plataforma, edad_Recomendada, precio)  
-VALUES  
-('The Last of Us', 'Naughty Dog', 'Aventura', 'PS4', 18, 49.99),  
-('God of War', 'Santa Monica Studio', 'Acción', 'PS5', 18, 59.99),  
-('Uncharted 4', 'Naughty Dog', 'Acción/Aventura', 'PS4', 16, 39.99),  
-('Spider-Man', 'Insomniac Games', 'Acción', 'PS4', 16, 44.99),  
-('Horizon Zero Dawn', 'Guerrilla Games', 'Acción/RPG', 'PS4', 16, 34.99);
+INSERT INTO Prestamos (
+    tipo_producto, id_producto, fecha_prestamo, fecha_devolucion, nombre_usuario
+) VALUES (
+    'JuegoPlayStation', 4, '2025-04-18 17:45:00', '2025-05-02 17:45:00', 'Andrea Torres'
+);
